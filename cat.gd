7132e931f330123577a8a8f321c8b071665e74e7
mattvonfat <matt@mattvonfat.com>
2023-12-09 20:32:18 +0000

    Initial commit

---
+++ cat.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+const MOVE_SPEED:float = 50.0
+
+var path:PackedVector3Array
+var target:Vector2
+var has_target:bool = false
+
+var cat_vertical = 0 # this is the current vertical position of the cat
+var target_vertical # this will be the vertical position of the target position
+
+func set_path(new_path:PackedVector3Array):
+	path = new_path
+	if path.size() > 0:
+		target_vertical = path[0].y # save the vertcial position of the target point
+		target = v3_to_v2(path[0]) # convert the targer position to a 2D position
+		path.remove_at(0) # remove the target position from the path
+		has_target = true # 
+
+func _physics_process(_delta):
+	if has_target:
+		# see if we have reached the target - I just guessed at 2 being ok
+		if position.distance_to(target) < 2:
+			# check if there are more points in the path
+			if path.size() > 0:
+				# update the cat vertical position
+				cat_vertical = target_vertical
+				# get the new target/remove path/etc
+				target = v3_to_v2(path[0])
+				target_vertical = path[0].y
+				path.remove_at(0)
+				velocity = position.direction_to(target)*MOVE_SPEED
+			else:
+				# no more points in path so target is reached
+				has_target = false
+				velocity = Vector2.ZERO
+		else:
+			velocity = position.direction_to(target)*MOVE_SPEED
+	
+		move_and_slide()
+
+# returns the 3D positon of the cat
+func get_pos3D():
+	var v = Vector3()
+	v.x = position.x
+	v.z = position.y
+	v.y = cat_vertical
+	return v
+
+# takes a 3D vector and returns the x and z coords as a 2D vector
+func v3_to_v2(v3:Vector3):
+	var v2 = Vector2()
+	v2.x = v3.x
+	v2.y = v3.z
+	return v2
---
+++ game.gd
@@ -0,0 +1,191 @@
+extends Node2D
+
+enum { TILE_FLAT=0, TILE_RAMP_NE=1, TILE_RAMP_SE=2, TILE_RAMP_SW=3, TILE_RAMP_NW=4 }
+
+@onready var tilemap:TileMap = $TileMap
+@onready var cat = $Cat
+
+const LAYER_HEIGHT:int = 16
+
+var layer_count:int # number of layers in the tile map
+var map_layer_collection:Dictionary # stores the tile data for each layer
+
+var nav_map # navigation map that we will create
+
+func _ready():
+	generate_nav_mesh()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.pressed == true:
+			var global_mouse = get_global_mouse_position()
+			var clicked_tile = tilemap.local_to_map(global_mouse) # map coordinate of tile
+			
+			# go through the layers from top to bottom until we find a layer which has a tile at the clicked position
+			for layer_id in range(layer_count-1, -1, -1):
+				if map_layer_collection[layer_id].has(clicked_tile):
+					# turn the clicked 2D position in to a 3D position
+					var clicked_pos3D = Vector3(global_mouse.x, layer_id*LAYER_HEIGHT, global_mouse.y)
+					# get the 3D position of the character
+					var character_pos3D = cat.get_pos3D()
+					# get the path from the NavigationServer3D
+					var path:PackedVector3Array = NavigationServer3D.map_get_path(nav_map, character_pos3D, clicked_pos3D, true)
+					# pass the path to the character
+					cat.set_path(path)
+					break
+
+
+func generate_nav_mesh():
+	var new_mesh:NavigationMesh = NavigationMesh.new()
+	
+	layer_count = tilemap.get_layers_count()
+	map_layer_collection = {}
+	
+	# get the tile map data for each layer and store in map_collection_layer
+	for layer_id in range(layer_count):
+		map_layer_collection[layer_id] = tilemap.get_used_cells(layer_id)
+	
+	
+	for layer_id in range(layer_count):
+		# go through each tile in the layer
+		for tile in map_layer_collection[layer_id]:
+			
+			# need to check if there is a tile above this tile, if so don't add it to the nav mesh
+			if layer_id+1 < layer_count:
+				# the tile above this tile will have the same x coord but the y coord will be 2 smaller
+				var stacked_tile = tile
+				stacked_tile.y -= 2
+				
+				if map_layer_collection[layer_id+1].has(stacked_tile):
+					# there is a tile above this one so move to next one
+					continue
+			
+			# get the "tile_type" of the current tile so we know how to create it
+			var tile_data = tilemap.get_cell_tile_data(layer_id, tile)
+			var tile_type = tile_data.get_custom_data("tile_type")
+			
+			match tile_type:
+				0: #flat
+					add_flat_polygons(tile, layer_id, new_mesh)
+				
+				3: # SW ramp
+					add_sw_ramp_polygons(tile, layer_id, new_mesh)
+	
+	# save the nav mesh - only done so it can be viewed outside of the program for debug purposes
+	var save_result = ResourceSaver.save(new_mesh, "nav_mesh.tres", 1)
+	print("Nav mesh save result: %s" % save_result)
+	
+	# create a new navigation map
+	nav_map = NavigationServer3D.map_create()
+	
+	# create a new navigation region and add the mesh to it, then add the region to the map
+	var new_region = NavigationServer3D.region_create()
+	NavigationServer3D.region_set_navigation_mesh(new_region, new_mesh)
+	NavigationServer3D.region_set_map(new_region, nav_map)
+	
+	# make the new map the active map
+	NavigationServer3D.map_set_active(nav_map, true)
+
+
+
+func add_flat_polygons(tile:Vector2i, layer:int, mesh:NavigationMesh):
+	# get the array of vertices from the nav mesh
+	var vertices:PackedVector3Array = mesh.get_vertices()
+	
+	# calculate the coordinates of the four vertices
+	var v1:Vector3
+	var v2:Vector3
+	var v3:Vector3
+	var v4:Vector3
+	
+	# because of the way the isometric tile map is laid out, the position of the tiles can vary depending on
+	# whether the coords are odd or even
+	if tile.y % 2 == 0:
+		v1 = Vector3( (tile[0]*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # left
+		v2 = Vector3( 16+(tile[0]*32), layer*LAYER_HEIGHT, ((tile[1])*8) ) # top
+		v3 = Vector3( ((tile[0]+1)*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # right
+		v4 = Vector3( 16+(tile[0]*32), layer*LAYER_HEIGHT, 16+((tile[1])*8) ) # bottom
+	else:
+		v1 = Vector3( 16+(tile[0]*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # left
+		v2 = Vector3( 32+(tile[0]*32), layer*LAYER_HEIGHT, ((tile[1])*8) ) # top
+		v3 = Vector3( 16+((tile[0]+1)*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # right
+		v4 = Vector3( 32+(tile[0]*32), layer*LAYER_HEIGHT, 16+((tile[1])*8) ) # bottom
+	
+	# this ugly section checks if the vertices already exist in the nav mesh vertices array
+	# if the don't exist then we add them
+	var a = vertices.find(v1)
+	if a == -1:
+		vertices.append(v1)
+	
+	a = vertices.find(v2)
+	if a == -1:
+		vertices.append(v2)
+	
+	a = vertices.find(v3)
+	if a == -1:
+		vertices.append(v3)
+	
+	a = vertices.find(v4)
+	if a == -1:
+		vertices.append(v4)
+	
+	# we set the updated vertices array to the nav mesh
+	mesh.set_vertices(vertices)
+	# we now add the two polygons that make up this tile, they are defined using the indexes of the vertices
+	# so we find the index for each value we want to use from the vertcies array
+	mesh.add_polygon(PackedInt32Array([vertices.find(v1), vertices.find(v2), vertices.find(v3)]))
+	mesh.add_polygon(PackedInt32Array([vertices.find(v1), vertices.find(v3), vertices.find(v4)]))
+
+
+func add_sw_ramp_polygons(tile:Vector2i, layer:int, mesh:NavigationMesh):
+	# this funtion is similar to the above with a few chnages
+	var vertices:PackedVector3Array = mesh.get_vertices()
+	
+	var v1:Vector3
+	var v2:Vector3
+	var v3:Vector3
+	var v4:Vector3
+	
+	# the left and bottom vertices of this tile will be in the same position as the top and right vertices of the tile
+	# this ramp connects to, so we find the tile that this ramp connects to on the lower layer
+	var lower_tile = Vector2i()
+	lower_tile.x = tile.x
+	lower_tile.y = tile.y + 3
+	if tile.y % 2 == 0:
+		lower_tile.x -= 1
+	
+	# the top and right vertices depend on whether the y coordinate is odd or even as with the flat tile function
+	if tile.y % 2 == 0:
+		v2 = Vector3( 16+(tile[0]*32), layer*LAYER_HEIGHT, ((tile[1])*8) ) # top
+		v3 = Vector3( ((tile[0]+1)*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # right
+	else:
+		v2 = Vector3( 32+(tile[0]*32), layer*LAYER_HEIGHT, ((tile[1])*8) ) # top
+		v3 = Vector3( 16+((tile[0]+1)*32), layer*LAYER_HEIGHT, 8+((tile[1])*8) ) # right
+	
+	# the left and bottom vertices depend on whether the lower tile is odd of even
+	if lower_tile.y % 2 == 0:
+		v1 = Vector3( 16+(lower_tile[0]*32), (layer-1)*LAYER_HEIGHT, ((lower_tile[1])*8) ) # left (copied from flat top)
+		v4 = Vector3( ((lower_tile[0]+1)*32), (layer-1)*LAYER_HEIGHT, 8+((lower_tile[1])*8) ) # bottom (copied from flat right)
+	else:
+		v1 = Vector3( 32+(lower_tile[0]*32), (layer-1)*LAYER_HEIGHT, ((lower_tile[1])*8) ) # left (copied from flat top)
+		v4 = Vector3( 16+((lower_tile[0]+1)*32), (layer-1)*LAYER_HEIGHT, 8+((lower_tile[1])*8) ) # bottom copied from flat right)
+	
+	var a = vertices.find(v1)
+	if a == -1:
+		vertices.append(v1)
+	
+	a = vertices.find(v2)
+	if a == -1:
+		vertices.append(v2)
+	
+	a = vertices.find(v3)
+	if a == -1:
+		vertices.append(v3)
+		
+	a = vertices.find(v4)
+	if a == -1:
+		vertices.append(v4)
+	
+	mesh.set_vertices(vertices)
+	mesh.add_polygon(PackedInt32Array([vertices.find(v1), vertices.find(v2), vertices.find(v3)]))
+	mesh.add_polygon(PackedInt32Array([vertices.find(v1), vertices.find(v3), vertices.find(v4)]))
